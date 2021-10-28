77d09208ae9522fd28ecd15c03b0f10b6ed60e8d
yaattzy <yeratzyen@gmail.com>
2021-10-28 13:57:55 -0600

    Vuex: state

---
+++ 15-vue04/proyecto-vuex/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+   <h4>{{ $store.state.total }}</h4>
+   <h3 :style="colorTotal">{{total}}</h3>
+  </div>
+</template>
+
+<script>
+import {mapState} from "vuex" 
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data() {
+    return {
+      prop1: ""
+    }
+  },
+  computed: {
+    ...mapState(["total"]),
+    colorTotal(){
+      return this.total >= 100 ? {'color': 'green'} : {'color': 'red'};
+    }
+  }
+}
+</script>
